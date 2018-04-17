53f8cae003ccab9fa692a56ebd410473dcfa23cc
Gledson Rodrigues <ggledson@gmail.com>
2018-04-16 22:15:28 -0300

    Acrescentada a desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,7 +8,8 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Boa noite!");
+            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
         }
     }
